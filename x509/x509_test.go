bea86c96e38e8e33e93d09799d79a5950de93fd4
leijacob <lei.jacob@gmail.com>
2025-03-27 13:46:57 +0800

    update

--- x509/x509_test.go
+++ x509/x509_test.go
@@ -25,6 +25,7 @@ import (
 	"encoding/base64"
 	"fmt"
 	"math/big"
+	mrand "math/rand"
 	"net"
 	"reflect"
 	"testing"
@@ -52,6 +53,230 @@ MIIGXjCCBgOgAwIBAgINKmdqIzHOjZMIDu8YYzAMBggqgRzPVQGDdQUAMGExCzAJBgNVBAYMAkNOMQ0w
 			t.Fatal("Verify failed")
 		}
 	}
+}
+
+// GenerateFixedLengthRandomBigInt 生成指定长度的随机 big.Int 数
+func generateFixedLengthRandomBigInt(length int) *big.Int {
+	if length <= 0 {
+		return big.NewInt(0)
+	}
+	// 计算最小值和最大值
+	min := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(length-1)), nil)
+	max := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(length)), nil)
+	max.Sub(max, big.NewInt(1))
+
+	// 初始化随机数种子
+	mrand.Seed(time.Now().UnixNano())
+
+	// 生成随机数
+	randomNum := new(big.Int).Rand(mrand.New(mrand.NewSource(time.Now().UnixNano())), max)
+	// 确保随机数大于等于最小值
+	if randomNum.Cmp(min) < 0 {
+		randomNum.Add(randomNum, min)
+	}
+	return randomNum
+}
+
+func TestGenerateRootCert(t *testing.T) {
+	priv, err := sm2.GenerateKey(nil) // 生成密钥对
+	if err != nil {
+		t.Fatal(err)
+	}
+	privPem, err := WritePrivateKeyToPem(priv, nil) // 生成密钥文件
+	if err != nil {
+		t.Fatal(err)
+	}
+	// pubKey, _ := priv.Public().(*sm2.PublicKey)
+
+	// testExtKeyUsage := []ExtKeyUsage{ExtKeyUsageClientAuth, ExtKeyUsageServerAuth}
+	// testUnknownExtKeyUsage := []asn1.ObjectIdentifier{[]int{1, 2, 3}, []int{2, 59, 1}}
+	// extraExtensionData := []byte("extra extension")
+	commonName := "Elan SM2 Root CA"
+	template := Certificate{
+		// SerialNumber is negative to ensure that negative
+		// values are parsed. This is due to the prevalence of
+		// buggy code that produces certificates with negative
+		// serial numbers.
+		SerialNumber: generateFixedLengthRandomBigInt(32),
+		Subject: pkix.Name{
+			CommonName:         commonName,
+			Organization:       []string{"Elan"},
+			OrganizationalUnit: []string{"Elan"},
+			Country:            []string{"CN"},
+			// ExtraNames: []pkix.AttributeTypeAndValue{
+			// 	{ // G
+			// 		Type:  []int{2, 5, 4, 42},
+			// 		Value: "Gopher",
+			// 	},
+			// 	// This should override the Country, above.
+			// 	{ // C
+			// 		Type:  []int{2, 5, 4, 6},
+			// 		Value: "NL",
+			// 	},
+			// },
+		},
+		NotBefore: time.Now(),
+		NotAfter:  time.Now().Add(24 * 5 * 365 * time.Hour),
+
+		//		SignatureAlgorithm: ECDSAWithSHA256,
+		SignatureAlgorithm: SM2WithSM3,
+
+		// SubjectKeyId: []byte{1, 2, 3, 4},
+		SubjectKeyId: generateFixedLengthRandomBigInt(40).Bytes(),
+		KeyUsage:     KeyUsageCertSign | KeyUsageCRLSign,
+
+		// ExtKeyUsage:        testExtKeyUsage,
+		// UnknownExtKeyUsage: testUnknownExtKeyUsage,
+
+		BasicConstraintsValid: true,
+		IsCA:                  true,
+
+		// OCSPServer:            []string{"http://ocsp.example.com"},
+		// IssuingCertificateURL: []string{"http://crt.example.com/ca1.crt"},
+
+		// DNSNames:       []string{"test.example.com"},
+		// EmailAddresses: []string{"gopher@golang.org"},
+		// IPAddresses:    []net.IP{net.IPv4(127, 0, 0, 1).To4(), net.ParseIP("2001:4860:0:2001::68")},
+
+		// PolicyIdentifiers:   []asn1.ObjectIdentifier{[]int{1, 2, 3}},
+		// PermittedDNSDomains: []string{".example.com", "example.com"},
+
+		// CRLDistributionPoints: []string{"http://crl1.example.com/ca1.crl", "http://crl2.example.com/ca1.crl"},
+
+		ExtraExtensions: []pkix.Extension{
+			// {
+			// 	Id:    []int{1, 2, 3, 4},
+			// 	Value: extraExtensionData,
+			// },
+			// This extension should override the SubjectKeyId, above.
+			// {
+			// 	Id:       oidExtensionSubjectKeyId,
+			// 	Critical: false,
+			// 	Value:    []byte{0x04, 0x04, 4, 3, 2, 2},
+			// },
+		},
+	}
+	pubKey, _ := priv.Public().(*sm2.PublicKey)
+	certpem, err := CreateCertificateToPem(&template, &template, pubKey, priv)
+	if err != nil {
+		t.Fatal("failed to create cert file")
+	}
+	fmt.Println("privateKey: ", string(privPem))
+	fmt.Println("cert: ", string(certpem))
+	cert, err := ReadCertificateFromPem(certpem)
+	if err != nil {
+		t.Fatal("failed to read cert file")
+	}
+	err = cert.CheckSignature(cert.SignatureAlgorithm, cert.RawTBSCertificate, cert.Signature)
+	if err != nil {
+		t.Fatal(err)
+	} else {
+		fmt.Printf("CheckSignature ok\n")
+	}
+}
+
+func TestApplyCert(t *testing.T) {
+	csr := `-----BEGIN CERTIFICATE REQUEST-----
+MIIBVjCB+wIBADCBmDELMAkGA1UEBgwCQ04xEjAQBgNVBAgMCeaxn+iLj+ecgTESMBAGA1UEBwwJ5Y2X5Lqs5biCMScwJQYDVQQKDB7msZ/oi4/nnIHkuK3opb/ljLvnu5PlkIjljLvpmaIxDzANBgNVBAsMBumZouWKnjEnMCUGA1UEAwwe5rGf6IuP55yB5Lit6KW/5Yy757uT5ZCI5Yy76ZmiMFkwEwYHKoZIzj0CAQYIKoEcz1UBgi0DQgAEikftgnOwk1zyeYauznCtv810hRrcmftu8urYnEJcYOvwQak4HEmdb/KyF+rgmYEJBDxevXZcYP1puxEB8yrYmaAAMAwGCCqBHM9VAYN1BQADSAAwRQIgB1ZWebL0SAYFC0n83TKWmbbrs5upa5U4SVh+xwigiZkCIQCwabFwhbo/DuvUJbZyRvOnCCfgctp/vQI4+RRncTFoZA==
+-----END CERTIFICATE REQUEST-----`
+	pk := `-----BEGIN PRIVATE KEY-----
+MIGTAgEAMBMGByqGSM49AgEGCCqBHM9VAYItBHkwdwIBAQQgxfsQBGooRlI7etP4
+HZB9enwh5lutYqzg7mJzUqJfA9ygCgYIKoEcz1UBgi2hRANCAATaDZlEdmfUa4BT
+lZfDDxk7uCTCSKLaXBDVWVcIc9ufUgBKXV080dOTdlI9aa/xS+u1YESukFjeUSkb
+i35mw8Ev
+-----END PRIVATE KEY-----`
+	cert := `-----BEGIN CERTIFICATE-----
+MIIBxzCCAW2gAwIBAgIOAefo6gqYdh0v8vmU9jAwCgYIKoEcz1UBg3UwRjELMAkG
+A1UEBhMCQ04xDTALBgNVBAoTBEVsYW4xDTALBgNVBAsTBEVsYW4xGTAXBgNVBAMT
+EEVsYW4gU00yIFJvb3QgQ0EwHhcNMjUwMjI3MDcxNDQwWhcNMzAwMjI2MDcxNDQw
+WjBGMQswCQYDVQQGEwJDTjENMAsGA1UEChMERWxhbjENMAsGA1UECxMERWxhbjEZ
+MBcGA1UEAxMQRWxhbiBTTTIgUm9vdCBDQTBZMBMGByqGSM49AgEGCCqBHM9VAYIt
+A0IABNoNmUR2Z9RrgFOVl8MPGTu4JMJIotpcENVZVwhz259SAEpdXTzR05N2Uj1p
+r/FL67VgRK6QWN5RKRuLfmbDwS+jPzA9MA4GA1UdDwEB/wQEAwIBBjAPBgNVHRMB
+Af8EBTADAQH/MBoGA1UdDgQTBBEQv3p9gz0xVAaLhUbykz8LnzAKBggqgRzPVQGD
+dQNIADBFAiEAtG1VNxc1GFZkrAHPVWvAPTQ4bPNZs/6iXz1ijKaDbFMCIGeLbCsz
+uDOE4TN2a+ewPRSWEkZ1pKqNZ+IEWWAN6+PJ
+-----END CERTIFICATE-----`
+
+	privateKey, err := ReadPrivateKeyFromPem([]byte(pk), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	requestPem, err := ReadCertificateRequestFromPem([]byte(csr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	rootCert, err := ReadCertificateFromPem([]byte(cert))
+	if err != nil {
+		t.Fatal(err)
+	}
+	// testExtKeyUsage := []ExtKeyUsage{ExtKeyUsageClientAuth, ExtKeyUsageServerAuth}
+	oid37 := make([]byte, 2+len("12100000400013118R"))
+	copy(oid37, []byte{0xc, 0x12})
+	copy(oid37[2:], []byte("12100000400013118R"))
+	template := Certificate{
+		// SerialNumber is negative to ensure that negative
+		// values are parsed. This is due to the prevalence of
+		// buggy code that produces certificates with negative
+		// serial numbers.
+		SerialNumber: generateFixedLengthRandomBigInt(32),
+		Subject:      requestPem.Subject,
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().Add(24 * 365 * time.Hour),
+
+		//		SignatureAlgorithm: ECDSAWithSHA256,
+		SignatureAlgorithm: SM2WithSM3,
+
+		SubjectKeyId: generateFixedLengthRandomBigInt(40).Bytes(),
+		KeyUsage:     KeyUsageDigitalSignature | KeyUsageContentCommitment,
+
+		// ExtKeyUsage: testExtKeyUsage,
+		// UnknownExtKeyUsage: testUnknownExtKeyUsage,
+
+		BasicConstraintsValid: false,
+		// IsCA:                  true,
+
+		// OCSPServer:            []string{"http://ocsp.example.com"},
+		// IssuingCertificateURL: []string{"http://crt.example.com/ca1.crt"},
+
+		// DNSNames:       []string{"test.example.com"},
+		// EmailAddresses: []string{"gopher@golang.org"},
+		// IPAddresses:    []net.IP{net.IPv4(127, 0, 0, 1).To4(), net.ParseIP("2001:4860:0:2001::68")},
+
+		// PolicyIdentifiers:   []asn1.ObjectIdentifier{[]int{1, 2, 3}},
+		// PermittedDNSDomains: []string{".example.com", "example.com"},
+
+		// CRLDistributionPoints: []string{"http://crl1.example.com/ca1.crl", "http://crl2.example.com/ca1.crl"},
+
+		ExtraExtensions: []pkix.Extension{
+			{
+				Id:    []int{1, 2, 156, 112562, 2, 1, 1, 1},
+				Value: oid37,
+			},
+			{
+				Id:    []int{1, 2, 156, 112562, 2, 1, 1, 37},
+				Value: oid37,
+			},
+			{
+				Id:    []int{1, 2, 156, 112562, 2, 1, 1, 30},
+				Value: []byte{0x0c, 0x04, 0x31, 0x38, 0x31, 0x37},
+			},
+			// This extension should override the SubjectKeyId, above.
+			{
+				Id:       oidExtensionSubjectKeyId,
+				Critical: false,
+				Value:    []byte{0x04, 0x04, 4, 3, 2, 1},
+			},
+		},
+	}
+	key := requestPem.PublicKey.(*ecdsa.PublicKey)
+	sm2key := &sm2.PublicKey{X: key.X, Y: key.Y, Curve: key.Curve}
+	certpem, err := CreateCertificateToPem(&template, rootCert, sm2key, privateKey)
+	if err != nil {
+		t.Fatal("failed to create cert file")
+	}
+	fmt.Println(string(certpem))
+
 }
 
 func TestX509(t *testing.T) {
@@ -98,17 +323,18 @@ func TestX509(t *testing.T) {
 	testExtKeyUsage := []ExtKeyUsage{ExtKeyUsageClientAuth, ExtKeyUsageServerAuth}
 	testUnknownExtKeyUsage := []asn1.ObjectIdentifier{[]int{1, 2, 3}, []int{2, 59, 1}}
 	extraExtensionData := []byte("extra extension")
-	commonName := "test.example.com"
+	commonName := "Elan SM2 CA"
 	template := Certificate{
 		// SerialNumber is negative to ensure that negative
 		// values are parsed. This is due to the prevalence of
 		// buggy code that produces certificates with negative
 		// serial numbers.
-		SerialNumber: big.NewInt(-1),
+		SerialNumber: generateFixedLengthRandomBigInt(20),
 		Subject: pkix.Name{
-			CommonName:   commonName,
-			Organization: []string{"TEST"},
-			Country:      []string{"China"},
+			CommonName:         commonName,
+			Organization:       []string{"Elan"},
+			OrganizationalUnit: []string{"Elan"},
+			Country:            []string{"CN"},
 			ExtraNames: []pkix.AttributeTypeAndValue{
 				{
 					Type:  []int{2, 5, 4, 42},
@@ -122,13 +348,14 @@ func TestX509(t *testing.T) {
 			},
 		},
 		NotBefore: time.Now(),
-		NotAfter:  time.Date(2021, time.October, 10, 12, 1, 1, 1, time.UTC),
+		NotAfter:  time.Now().Add(24 * 5 * 365 * time.Hour),
 
 		//		SignatureAlgorithm: ECDSAWithSHA256,
 		SignatureAlgorithm: SM2WithSM3,
 
-		SubjectKeyId: []byte{1, 2, 3, 4},
-		KeyUsage:     KeyUsageCertSign,
+		// SubjectKeyId: []byte{1, 2, 3, 4},
+		SubjectKeyId: generateFixedLengthRandomBigInt(20).Bytes(),
+		KeyUsage:     KeyUsageCertSign | KeyUsageCRLSign,
 
 		ExtKeyUsage:        testExtKeyUsage,
 		UnknownExtKeyUsage: testUnknownExtKeyUsage,
@@ -154,11 +381,11 @@ func TestX509(t *testing.T) {
 				Value: extraExtensionData,
 			},
 			// This extension should override the SubjectKeyId, above.
-			{
-				Id:       oidExtensionSubjectKeyId,
-				Critical: false,
-				Value:    []byte{0x04, 0x04, 4, 3, 2, 1},
-			},
+			// {
+			// 	Id:       oidExtensionSubjectKeyId,
+			// 	Critical: false,
+			// 	Value:    []byte{0x04, 0x04, 4, 3, 2, 2},
+			// },
 		},
 	}
 	pubKey, _ = priv.Public().(*sm2.PublicKey)
@@ -166,6 +393,9 @@ func TestX509(t *testing.T) {
 	if err != nil {
 		t.Fatal("failed to create cert file")
 	}
+	fmt.Println("csr: ", string(reqPem))
+	fmt.Println("cert: ", string(certpem))
+	fmt.Println("private key: :", string(privPem))
 	cert, err := ReadCertificateFromPem(certpem)
 	if err != nil {
 		t.Fatal("failed to read cert file")
